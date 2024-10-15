04816c764441b37d8e8d31e64c9e7cdc41f8e7c7
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-10-15 16:41:51 +0200

    REVERT last commit

--- js/script.js
+++ js/script.js
@@ -10,32 +10,14 @@ for (let i = 0; i < 100; i++) {
     let remainder3 = num % 3
     let remainder5 = num % 5
 
-    // Inizializzo variabili booleane per il controllo del resto
-    let isRem3Zero = false
-    let isRem5Zero = false
-
-    // Modifico il contenuto delle variabili booleane in base al contenuto
-    if (remainder3 === 0) {
-        isRem3Zero = true
-    } else {
-        isRem3Zero = false
-    }
-
-    if (remainder5 === 0) {
-        isRem5Zero = true
-    } else {
-        isRem5Zero = false
-    }
-
     // Controllo se il numero da stampare è divisibile per 3, per 5, entrambi o nessuno dei due e in base al risultato stampo rispettivamente: Fizz, Buzz, FizzBuzz o il numero stesso
-    // Utilizzo le variabili booleane per una migliore leggibilità del codice
-    if (isRem3Zero && !isRem5Zero) {
+    if (remainder3 === 0 && remainder5 !== 0) {
         console.log("Fizz!")
-    } else if (!isRem3Zero && isRem5Zero) {
+    } else if (remainder3 !== 0 && remainder5 === 0) {
         console.log("Buzz!")
-    } else if (isRem3Zero && isRem5Zero) {
+    } else if (remainder3 === 0 && remainder5 === 0) {
         console.log("FizzBuzz!")
-    } else if (!isRem3Zero && !isRem5Zero) {
+    } else if (remainder3 !== 0 && remainder5 !== 0) {
         console.log(num)
     }
 }
