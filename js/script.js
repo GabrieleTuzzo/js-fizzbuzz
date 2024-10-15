638c7c643ed31bfa333e0303cb963d41ad5809ec
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-10-15 16:27:35 +0200

    ADD Boolean control variable for better code readability

--- js/script.js
+++ js/script.js
@@ -10,14 +10,32 @@ for (let i = 0; i < 100; i++) {
     let remainder3 = num % 3
     let remainder5 = num % 5
 
+    // Inizializzo variabili booleane per il controllo del resto
+    let isRem3Zero = false
+    let isRem5Zero = false
+
+    // Modifico il contenuto delle variabili booleane in base al contenuto
+    if (remainder3 === 0) {
+        isRem3Zero = true
+    } else {
+        isRem3Zero = false
+    }
+
+    if (remainder5 === 0) {
+        isRem5Zero = true
+    } else {
+        isRem5Zero = false
+    }
+
     // Controllo se il numero da stampare è divisibile per 3, per 5, entrambi o nessuno dei due e in base al risultato stampo rispettivamente: Fizz, Buzz, FizzBuzz o il numero stesso
-    if (remainder3 === 0 && remainder5 !== 0) {
+    // Utilizzo le variabili booleane per una migliore leggibilità del codice
+    if (isRem3Zero && !isRem5Zero) {
         console.log("Fizz!")
-    } else if (remainder3 !== 0 && remainder5 === 0) {
+    } else if (!isRem3Zero && isRem5Zero) {
         console.log("Buzz!")
-    } else if (remainder3 === 0 && remainder5 === 0) {
+    } else if (isRem3Zero && isRem5Zero) {
         console.log("FizzBuzz!")
-    } else if (remainder3 !== 0 && remainder5 !== 0) {
+    } else if (!isRem3Zero && !isRem5Zero) {
         console.log(num)
     }
 }
